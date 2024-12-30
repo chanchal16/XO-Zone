0cdefa95bb45637acf1d4e1f4486178c00b34fa9
chanchal16 <rajputchanchal17@gmail.com>
2024-12-30 17:30:10 +0530

    refactor: add dropdown for difficulty level

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -4,6 +4,7 @@ import { Button } from "@/components/ui/button";
 import {
   resetGame,
   restartGame,
+  selectDifficulty,
 } from "@/lib/features/single-player/singlePlayerSlice";
 import { useAppDispatch, useAppSelector } from "@/lib/hooks";
 import { useRouter } from "next/navigation";
@@ -11,7 +12,7 @@ import React from "react";
 
 const SinglePlayerGame = () => {
   const singlePlayerState = useAppSelector((state) => state.singleMode);
-  const { winner, score, isDraw } = singlePlayerState;
+  const { winner, score, isDraw, difficulty } = singlePlayerState;
   const dispatch = useAppDispatch();
   const router = useRouter();
 
@@ -20,8 +21,26 @@ const SinglePlayerGame = () => {
     router.push("/");
   };
 
+  const handleChange = (e: any) => {
+    dispatch(selectDifficulty(e.target.value));
+  };
+
   return (
     <div className="flex flex-col items-center justify-items-center min-h-screen p-8 pb-20 gap-4 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <div>
+        <label htmlFor="difficulty">Choose a difficulty:</label>
+
+        <select
+          name="difficulty"
+          id="difficulty"
+          value={difficulty}
+          onChange={handleChange}
+        >
+          <option value="easy">Easy</option>
+          <option value="medium">Medium</option>
+          <option value="hard">Hard</option>
+        </select>
+      </div>
       <div className="grid grid-cols-3 gap-2 w-60">
         <Board />
       </div>
