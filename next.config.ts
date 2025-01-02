2d1ad82aab0397f4d38376a3582f0c42ce6a9a1c
chanchal16 <rajputchanchal17@gmail.com>
2025-01-02 18:18:46 +0530

    chore(config): Add multiavatar domain to next.js image configuration

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "api.multiavatar.com",
+        port: "",
+        pathname: "/**", // Allow all paths under api.dicebear.com
+      },
+    ],
+  },
 };
 
 export default nextConfig;
