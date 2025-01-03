adb4bc9933140398bdbc642eb5d34156fadcac5e
chanchal16 <rajputchanchal17@gmail.com>
2025-01-03 18:54:10 +0530

    style: change label position

--- src/components/PlayerInfo.tsx
+++ src/components/PlayerInfo.tsx
@@ -4,26 +4,26 @@ import Image from "next/image";
 
 const PlayerInfo = ({ player, bot, avatar }: IPlayer) => {
   return (
-    <div className="flex gap-12 text-lg mb-4">
-      <div className="flex flex-col items-center">
+    <div className="flex gap-16 text-lg mb-8">
+      <div className="flex flex-col items-center px-2 relative">
         <Image
           src={"https://api.multiavatar.com/Broomhilda.svg"}
           alt="avatar"
           width={100}
           height={100}
         />
-        <span className="p-2 bg-minimal-playerO-100 text-minimal-playerX-200 rounded-md text-sm">
+        <span className="py-1.5 px-4 absolute -bottom-3.5 bg-minimal-playerX-100 text-minimal-accentC rounded-md text-sm">
           You are {player}
         </span>
       </div>
-      <div className="flex flex-col items-center">
+      <div className="flex flex-col items-center relative">
         <Image
           src={"https://api.multiavatar.com/Skeleto.svg"}
           alt="bot"
           width={100}
           height={100}
         />
-        <span className="p-2 bg-minimal-playerO-100 text-minimal-playerX-200 rounded-md text-sm">
+        <span className="py-1.5 px-4 absolute -bottom-3.5 bg-minimal-playerX-100 text-minimal-accentC rounded-md text-sm">
           Bot is {bot}
         </span>
       </div>
