fa2e530bc61dc4f7dadec5c6940118f86c956661
chanchal16 <rajputchanchal17@gmail.com>
2025-01-05 19:02:29 +0530

    refactor: move results code to ResultModal component

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -1,24 +1,13 @@
 "use client";
 import Board from "@/components/Board";
-import { Button } from "@/components/ui/button";
-import {
-  resetGame,
-  restartGame,
-} from "@/lib/features/single-player/singlePlayerSlice";
-import { useAppDispatch, useAppSelector } from "@/lib/hooks";
-import { useRouter } from "next/navigation";
+import ResultModal from "@/components/ResultModal";
+import { useAppSelector } from "@/lib/hooks";
+
 import React from "react";
 
 const SinglePlayerGame = () => {
   const singlePlayerState = useAppSelector((state) => state.singleMode);
-  const { winner, isDraw } = singlePlayerState;
-  const dispatch = useAppDispatch();
-  const router = useRouter();
-
-  const handleQuit = () => {
-    dispatch(resetGame());
-    router.push("/");
-  };
+  const { winner, isDraw } = singlePlayerState; 
 
   return (
     <div className="flex flex-col items-center bg-minimal-playerO-100 text-minimal-textClr min-h-screen p-8 pb-20 gap-4 sm:p-20 font-[family-name:var(--font-poppins)]">
@@ -26,29 +15,8 @@ const SinglePlayerGame = () => {
         <Board />
       </div>
 
-      {winner && (
-        <h2 className="text-3xl">
-          Winner is - <span className="text-amber-600">{winner}</span>
-        </h2>
-      )}
+      <ResultModal />
       {isDraw && <div className="text-center mt-4">It's a Draw!</div>}
-
-      <div className="flex gap-6">
-        <Button
-          variant={"default"}
-          className=" text-minimal-gridLines font-semibold bg-minimal-accentC"
-          onClick={() => dispatch(restartGame())}
-        >
-          Restart
-        </Button>
-        <Button
-          variant={"destructive"}
-          className="bg-white border-2 border-minimal-playerO-200 text-minimal-playerO-200 hover:text-white hover:bg-minimal-playerO-200"
-          onClick={handleQuit}
-        >
-          Quit
-        </Button>
-      </div>
     </div>
   );
 };
