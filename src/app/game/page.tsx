2f8972cda3bf0e686584ee4beb9e486bde4473f8
chanchal16 <rajputchanchal17@gmail.com>
2025-01-05 17:25:39 +0530

    refactor: remove unwanted code

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -4,18 +4,10 @@ import { Button } from "@/components/ui/button";
 import {
   resetGame,
   restartGame,
-  selectDifficulty,
 } from "@/lib/features/single-player/singlePlayerSlice";
 import { useAppDispatch, useAppSelector } from "@/lib/hooks";
 import { useRouter } from "next/navigation";
 import React from "react";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
 
 const SinglePlayerGame = () => {
   const singlePlayerState = useAppSelector((state) => state.singleMode);
@@ -28,24 +20,8 @@ const SinglePlayerGame = () => {
     router.push("/");
   };
 
-  const handleChange = (value: any) => {
-    dispatch(selectDifficulty(value));
-  };
-
   return (
-    <div className="flex flex-col items-center bg-minimal-playerO-100 text-minimal-textClr min-h-screen p-8 pb-20 gap-4 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <div className="self-start">
-        <Select defaultValue="easy" onValueChange={handleChange}>
-          <SelectTrigger className="w-[120px]">
-            <SelectValue placeholder="Difficulty" />
-          </SelectTrigger>
-          <SelectContent className="">
-            <SelectItem value="easy">Easy</SelectItem>
-            <SelectItem value="medium">Medium</SelectItem>
-            <SelectItem value="hard">Hard</SelectItem>
-          </SelectContent>
-        </Select>
-      </div>
+    <div className="flex flex-col items-center bg-minimal-playerO-100 text-minimal-textClr min-h-screen p-8 pb-20 gap-4 sm:p-20 font-[family-name:var(--font-poppins)]">
       <div className="grid grid-cols-3 p-6 w-72 bg-minimal-board rounded-lg">
         <Board />
       </div>
@@ -60,14 +36,14 @@ const SinglePlayerGame = () => {
       <div className="flex gap-6">
         <Button
           variant={"default"}
-          className=" text-white font-semibold bg-minimal-accentC"
+          className=" text-minimal-gridLines font-semibold bg-minimal-accentC"
           onClick={() => dispatch(restartGame())}
         >
           Restart
         </Button>
         <Button
           variant={"destructive"}
-          className="bg-minimal-playerO-200 text-black"
+          className="bg-white border-2 border-minimal-playerO-200 text-minimal-playerO-200 hover:text-white hover:bg-minimal-playerO-200"
           onClick={handleQuit}
         >
           Quit
