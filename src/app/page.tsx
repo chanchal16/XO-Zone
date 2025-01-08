0b143c682ab1eb230bdc6f6e2f131c2b6c4e7449
chanchal16 <rajputchanchal17@gmail.com>
2025-01-08 16:19:35 +0530

    refactor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
 
   return (
     <div className="flex flex-col items-center  bg-minimal-gridLines text-minimal-textClr min-h-screen sm:min-h-max p-8 pb-20 gap-8 sm:p-20 font-[family-name:var(--font-poppins)]">
-      {!mode ? (
+      {!mode && (
         <>
           <h1 className="text-3xl font-semibold font-[family-name:var(--font-chewy)]">
             Choose a mode
@@ -24,37 +24,38 @@ export default function Home() {
           <div className="flex flex-col gap-8 ">
             <Button
               variant={"secondary"}
-              className="bg-white text-base text-minimal-playerO-200 p-4 hover:font-semibold"
+              className="bg-white text-base text-minimal-playerO-200 p-4 hover:text-white hover:bg-minimal-playerO-200"
               onClick={() => setMode("single")}
             >
               Single Player
             </Button>
-            <Button
+            {/* <Button
               variant={"secondary"}
               className="bg-white text-base text-minimal-playerO-200 p-4 hover:font-semibold"
-              // onClick={() => setMode('multi')}
+              onClick={() => setMode('multi')}
             >
               Multi Player
-            </Button>
+            </Button> */}
           </div>
         </>
-      ) : (
+      )}
+      {mode === "single" && (
         <>
           <h1 className="text-3xl font-semibold font-[family-name:var(--font-chewy)]">
             Pick your side
           </h1>
-          <div className="flex flex-col gap-8 ">
+          <div className="flex justify-between gap-12 ">
             <button
-              className="bg-white rounded-full p-4"
+              className="bg-white border-2 rounded-full p-4 hover:border-minimal-playerX-200"
               onClick={() => dispatch(selectSymbol("X"))}
             >
-              <Image src={"/x-mark.png"} alt="X-icon" width={44} height={44} />
+              <Image src={"/x-mark.png"} alt="X-icon" width={48} height={48} />
             </button>
             <button
-              className="bg-white rounded-full p-4"
+              className="bg-white border-2 rounded-full p-4 hover:border-minimal-playerX-200"
               onClick={() => dispatch(selectSymbol("O"))}
             >
-              <Image src={"/blue-o.png"} alt="O-icon" width={48} height={48} />
+              <Image src={"/blue-o.png"} alt="O-icon" width={52} height={52} />
             </button>
           </div>
           {playerSymbol && (
