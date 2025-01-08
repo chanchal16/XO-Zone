0f163c4a885cc7ab4de9afc6124038a235a9b87b
chanchal16 <rajputchanchal17@gmail.com>
2025-01-08 16:20:15 +0530

    style: layout the board at the center

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -6,7 +6,7 @@ import React from "react";
 const SinglePlayerGame = () => {
   return (
     // min-h-screen
-    <div className="flex flex-col items-center bg-minimal-playerO-100 text-minimal-textClr p-8 pb-20 gap-4 sm:p-20 font-[family-name:var(--font-poppins)]">
+    <div className="h-full flex items-center justify-center bg-minimal-playerO-100 text-minimal-textClr p-8 pb-20 gap-4 sm:p-20 font-[family-name:var(--font-poppins)]">
       <div className="grid grid-cols-3 p-6 w-80 bg-minimal-board rounded-lg">
         <Board />
       </div>
